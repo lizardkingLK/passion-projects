6530f94acfc0b55d4c042c424f7c809b2f28554f
lizardkinglk <chansanfdo@gmail.com>
2025-04-11 18:13:59 +0000

    removed automations.yaml, updated arguments

--- src/modules/listener.ts
+++ src/modules/listener.ts
@@ -1,19 +1,35 @@
 import { Canvas } from "./canvas";
+import { CANVAS_HEIGHT, CANVAS_WIDTH } from "./constants";
 import { validateInput } from "./operations";
+import { Shape } from "./types";
 
 export class Listener {
+  #shapes: Shape[];
+
+  constructor() {
+    this.#shapes = [];
+  }
+
   listen(event: Event, canvas: Canvas) {
-    const { isSuccess, message, data } = validateInput(
-      (<HTMLTextAreaElement>event.target).value
-    );
+    const {
+      isSuccess,
+      message,
+      data: treeStructure,
+    } = validateInput((<HTMLTextAreaElement>event.target).value);
 
     if (!isSuccess) {
       console.error(message);
+      return;
     }
 
-    const treeStructure = data;
     console.log(treeStructure);
-
-    canvas.drawCircle();
+    const circle = {
+      centerX: CANVAS_WIDTH / 2,
+      centerY: CANVAS_HEIGHT / 2,
+      radius: 20,
+    };
+    canvas.drawCircle(circle);
+    this.#shapes.push(circle);
+    console.log(this.#shapes);
   }
 }
