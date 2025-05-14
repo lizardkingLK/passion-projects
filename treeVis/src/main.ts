960243f9595f475bff35cbd6a25da8ff6be7972e
lizardkingLK <chansanfdo@gmail.com>
2025-05-14 11:53:18 +0530

    Updated styles
    - Updated id names with more suitable naming

--- treeVis/src/main.ts
+++ treeVis/src/main.ts
@@ -3,14 +3,14 @@ import { Events } from "./modules/events";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <section id="treeInputContainer" draggable="true">
-      <div id="treeInputHeading">Input</div>
+      <div id="treeInputHeader">Input</div>
       <textarea
         id="treeInput"
         placeholder="Enter tree info here..."
       ></textarea>
     </section>
     <section id="treeVisualContainer">
-      <div id="treeVisualHeading">Visual</div>
+      <div id="treeVisualHeader">Visual</div>
       <canvas id="treeVisual"></canvas>
     </section>
     <section id="treeVisualStatusContainer">
