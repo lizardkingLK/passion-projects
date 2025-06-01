6b860d34bc840e3546b6a1e2be8ca00055ad16d7
lizardkingLK <chansanfdo@gmail.com>
2025-06-01 12:38:18 +0530

    Updated components html
    - Added svgs by replacing images
    
    - Added help dialog box which contains descriptions to the user

--- treeVis/src/components/index.ts
+++ treeVis/src/components/index.ts
@@ -1,3 +1,5 @@
+import { closeIcon } from "./icons/close";
+
 const sectionTreeInputContainer = `
   <section id="treeInputContainer">
     <div id="treeInputHeader">
@@ -6,18 +8,40 @@ const sectionTreeInputContainer = `
         <button
           id="treeInputHeaderOptionRedraw"
           title="Redraw Tree">
-          <img
+          <svg
             id="treeInputHeaderOptionRedrawIcon"
-            src="./assets/images/refresh.svg"
-            alt="refresh" />
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m4.5 15.75 7.5-7.5 7.5 7.5"
+            />
+          </svg>
         </button>
         <button
           id="treeInputHeaderOptionFormat"
-          title="Format Text">
-          <img
+          title="Format Input">
+          <svg
             id="treeInputHeaderOptionFormatIcon"
-            src="./assets/images/format.svg"
-            alt="format" />
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5"
+            />
+          </svg>
         </button>
       </div>
     </div>
@@ -36,26 +60,64 @@ const sectionTreeVisualContainer = `
         <button
           id="treeVisualHeaderSave"
           title="Save Output">
-          <img
+          <svg
             id="treeVisualHeaderSaveIcon"
-            src="./assets/images/save.svg"
-            alt="save" />
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m9 13.5 3 3m0 0 3-3m-3 3v-6m1.06-4.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
+            />
+          </svg>
         </button>
         <button
           id="treeVisualHeaderHelp"
           title="View Help">
-          <img
+          <svg
             id="treeVisualHeaderHelpIcon"
-            src="./assets/images/help.svg"
-            alt="help" />
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+            />
+          </svg>
         </button>
         <button
           id="treeVisualHeaderSettings"
           title="View Settings">
-          <img
+          <svg
             id="treeVisualHeaderSettingsIcon"
-            src="./assets/images/cog.svg"
-            alt="settings" />
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+          </svg>
         </button>
       </div>
     </div>
@@ -67,6 +129,27 @@ const sectionTreeStatusContainer = `
   <section id="treeVisualStatusContainer"></section>
 `;
 
+const sectionTreeHelpContainer = `
+  <section id="treeHelpContainer" class="hidden">
+    <div id="treeHelpHeader">
+      <p>Help</p>
+      <button
+        id="treeHelpHeaderClose"
+        title="Close Help">
+        ${closeIcon("treeHelpHeaderCloseIcon")}
+      </button>
+    </div>
+    <div id="treeHelpContent"></div>
+    <div id="treeHelpFooter">
+      <button
+        id="treeHelpFooterClose"
+        title="Close Help">
+        <p>Close</p>
+      </button>
+    </div>
+  </section>
+`;
+
 const sectionTreeSettingsContainer = `
   <section id="treeSettingsContainer" class="hidden">
     <div id="treeSettingsHeader">
@@ -74,21 +157,18 @@ const sectionTreeSettingsContainer = `
       <button
         id="treeSettingsHeaderClose"
         title="Close Settings">
-        <img
-          id="treeSettingsHeaderCloseIcon" 
-          src="./assets/images/close.svg"
-          alt="close" />
+        ${closeIcon("treeSettingsHeaderCloseIcon")}
       </button>
     </div>
     <div id="treeSettingsContent"></div>
     <div id="treeSettingsFooter">
       <button
-        id="treeSettingsCancel"
+        id="treeSettingsFooterClose"
         title="Close Settings">
-        <p>Cancel</p>
+        <p>Close</p>
       </button>
       <button
-        id="treeSettingsSave"
+        id="treeSettingsFooterSave"
         title="Save Changes">
         <p>Save</p>
       </button>
@@ -102,6 +182,7 @@ export class Components {
     ${sectionTreeInputContainer}
     ${sectionTreeVisualContainer}
     ${sectionTreeStatusContainer}
+    ${sectionTreeHelpContainer}
     ${sectionTreeSettingsContainer}
     `;
   }
